98fb0ea4756dfa26d949ba9af4440e685d2db829
frostymaster <frostmasterwork@gmail.com>
2019-01-06 01:26:06 +0300

    1.0 version

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-**HELLO**
+**HELLO IT'S A ME**
 
-What's this u ask? This is just useless piece of software that advices anime based on your anime library
+Ok, so what's this you ask?
 
-It works with MyAnimeList API to fetch TOP20 animes atm and picks one based on your "tastes"
+This is a completely useless piece of software I've made due to educational purposes of my ML training, say hello and pay repsect.
 
-WIP. Gonna update this when I'm done
+What does it do? It recommends you what anime to watch based on your MyAnimeList list and what's popular today. **It only uses genres 'couse that's exactly what I wanted.**
+
+It works with unnoficial MyAnimeListApi - https://jikan.docs.apiary.io
+
+So, now I'm going to explain how it works, LET'S A-GO
+
+**STEPS**
+
+1) Fetch all animes from top20 list
+
+2) Fetch all user animes
+
+3) Create dataset
+
+4) Make prediction based on genres using Term Frequency (TF) and Inverse Document Frequency (IDF)
+
+5) Watch recommended anime.
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='
 
 link = "https://api.jikan.moe/v3/top/anime/1/tv"
 anime_link = "https://api.jikan.moe/v3/anime/"
+user_link = "https://api.jikan.moe/v3/user/Frost_Master/animelist/all" # change user name here
 
 resp = requests.get(link)
 
 animes = [[]]
 
-#create dataset
+# create dataset
 top = resp.json()['top']
 for top_anime in top :
     anime_resp = requests.get(anime_link + str(top_anime['mal_id']))
@@ -21,12 +22,25 @@ for top_anime in top :
     for genres in anime_resp.json()['genres']:
         genres_str = genres_str + genres['name'] + "|"
     genres_str = genres_str[:-1] #TODO this is gay fix this
-    animes.append([str(top_anime['title']), genres_str])
+    animes.append([str(top_anime['title']), genres_str, 1])
+
+resp = requests.get(user_link)
+users_anime = resp.json()['anime']
+user_anime_names = []
+
+for user_anime in users_anime:
+    anime_resp = requests.get(anime_link + str(user_anime['mal_id']))
+    genres_str = ""
+    for genres in anime_resp.json()['genres']:
+        genres_str = genres_str + genres['name'] + "|"
+    genres_str = genres_str[:-1]  # TODO this is gay fix this
+    animes.append([str(user_anime['title']), genres_str, 0])
+    user_anime_names.append([str(user_anime['title'])])
+
+# DATASET IS DONE OMG
 
 frame = pd.DataFrame(data=animes[1:],
-                     columns=['title', 'genres'])
-
-print(frame['genres'])
+                     columns=['title', 'genres', 'fromTop20'])
 
 tfidf_matrix = tf.fit_transform(frame['genres'])
 
@@ -35,12 +49,16 @@ cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 titles = frame['title']
 indices = pd.Series(frame.index, index=frame['title'])
 
-def genre_recommendations(title):
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:21]
-    movie_indices = [i[0] for i in sim_scores]
-    return titles.iloc[movie_indices]
 
-genre_recommendations('JoJo no Kimyou na Bouken: Ougon no Kaze').head(20)
+idx = indices['One Punch Man'] # PUT TITLE HERE
+sim_scores = list(enumerate(cosine_sim[idx]))
+sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+sim_scores = sim_scores[1:21]
+movie_indices = [i[0] for i in sim_scores]
+
+recommended_animes = titles.iloc[movie_indices]
+
+for anime in recommended_animes:
+    if str(anime) not in str(user_anime_names):
+        print(str(anime))
+        break
